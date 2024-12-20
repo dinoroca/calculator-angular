910e4a932078624a5c6bb0a28348c15b2eb49ced
Dino Roca <rocalaurented@gmail.com>
2024-12-19 19:36:32 -0500

    Feat: first commit, structure, buttons and styles of calculator

---
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular/core';
+
+@Component({
+  selector: 'calculator-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculator-button.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './calculator-button.component.css',
+  host: {
+    class: 'w-1/4 border-r border-b border-indigo-400'
+  }
+})
+export class CalculatorButtonComponent {
+
+  //Recepcion de signals y su trasformacion a boolean
+  public isCommand = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value
+  });
+
+  public isDoubleSize = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value
+  });
+
+  // @HostBinding('class.is-command') get commandStyle() {
+  //   return this.isCommand();
+  // }
+
+  @HostBinding('class.w-2/4') get commandStyle() {
+    return this.isDoubleSize();
+  }
+}
